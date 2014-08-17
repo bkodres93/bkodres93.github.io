8deab731f4a88e54b407155e75529f58b013767a
Ben Kodres-O'Brien <b.kodres@gmail.com>
2014-08-17 00:27:25 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+// $(function() {
+//     $('.page-scroll a').bind('click', function(event) {
+//        var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top
+//         }, 1000, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+// });
+
+
+$(document).ready(function () {
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 600);
+                return false;
+            }
+        }
+    });
+    insertEmail();
+});
+
+
+var insertEmail = function () {
+    var email = 'b.kodres@gmail.com';
+
+    $('#insert-email')
+        .attr('href', 'mailto:' + email)
+        .text(email);
+};
+
