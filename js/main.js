4f35371fe0da3f3484b50de429b4b388009247bd
bkodres93 <b.kodres@gmail.com>
2014-12-15 01:14:10 -0500

    Updated resume

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-// $(function() {
-//     $('.page-scroll a').bind('click', function(event) {
-//        var $anchor = $(this);
-//         $('html, body').stop().animate({
-//             scrollTop: $($anchor.attr('href')).offset().top
-//         }, 1000, 'easeInOutExpo');
-//         event.preventDefault();
-//     });
-// });
-
-
 $(document).ready(function () {
     $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
