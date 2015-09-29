a3003100468c92f7143bfb72fa679ca5c20fe13e
bkodres93 <b.kodres@gmail.com>
2015-09-28 22:52:45 -0400

    Added #art

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 $(document).ready(function () {
-    $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        && location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 600);
-                return false;
-            }
-        }
-    });
-    insertEmail();
+    var body = $('#content')
+    body.fadeTo(5000, 1, 'linear');
 });
 
 
